2a9b0e7231308f23571014cf148f49e2d655a706
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-11-02 18:23:00 -0300

    CSS do componente RODAPE

--- src/components/Rodape/Rodape.jsx
+++ src/components/Rodape/Rodape.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import Image from "next/image";
 
+import "./rodape.css"
+
+import Facebook from '../../app/assets/facebook.png'
+import Instagram from '../../app/assets/instagram.png'
+import Email from '../../app/assets/email.png'
 
 export default function Rodape() {
-  return (
-    <div>
-      <footer>
-        <p> @Grupo NTC</p>
-      </footer>
-    </div>
-  )
+    return(
+        <footer>
+            <div className='social-medias'>
+                <a className='social-media-logo' href="#"> <Image className="footer-img" src={Facebook} alt="Logo do facebook"/></a>
+                <a className='social-media-logo' href="#"> <Image className="footer-img" src={Email} alt="Logo do e-mail"/></a>
+                <a className='social-media-logo' href="#"> <Image className="footer-img" src={Instagram} alt="Logo do Instagram"/></a>
+            </div>
+        </footer>
+    )
 }
